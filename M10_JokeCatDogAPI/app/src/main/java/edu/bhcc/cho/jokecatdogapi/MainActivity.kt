659496af847aef6674aa7a4ca9a09a2eeaa7b380
Celia Ho <celia.ho+github@gmail.com>
2025-07-06 21:00:44 -0400

    add logic for button clicks, attempt matching status bar color to app bar color

--- M10_JokeCatDogAPI/app/src/main/java/edu/bhcc/cho/jokecatdogapi/MainActivity.kt
+++ M10_JokeCatDogAPI/app/src/main/java/edu/bhcc/cho/jokecatdogapi/MainActivity.kt
@@ -1,24 +1,48 @@
 package edu.bhcc.cho.jokecatdogapi
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.appbar.MaterialToolbar
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var lastSelectionTextView: TextView
+    private lateinit var jokeButton: MaterialButton
+    private lateinit var catButton: MaterialButton
+    private lateinit var dogButton: MaterialButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        // ??? No appreciable display difference (Prefab Android Studio code)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cat)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
 
-//        val toolbar = findViewById<MaterialToolbar>(R.id.appBar)
-//        setSupportActionBar(toolbar)
-//        supportActionBar?.title = getString(R.string.app_name)
+        // Initialize views
+        lastSelectionTextView = findViewById(R.id.tv_last_selection)
+        jokeButton = findViewById(R.id.btn_joke)
+        catButton = findViewById(R.id.btn_cat)
+        dogButton = findViewById(R.id.btn_dog)
+
+        // *HANDLE BUTTONS*
+        // Handle joke button click
+        jokeButton.setOnClickListener {
+            startActivity(Intent(this, JokeActivity::class.java))
+        }
+        // Handle cat button click
+        catButton.setOnClickListener {
+            startActivity(Intent(this, CatActivity::class.java))
+        }
+        // Handle dog button click
+        dogButton.setOnClickListener {
+            startActivity(Intent(this, DogActivity::class.java))
+        }
+
     }
 }
